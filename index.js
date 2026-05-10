e5a8c075742eeb83b6ff21ab809eebbdfa13c26a
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-05-10 19:13:48 +0530

    insert data

--- index.js
+++ index.js
@@ -1,19 +1,63 @@
 /*topic 1
 use faker to generate fake data
 use npm i @faker-js/faker for install package */
+/*topic 2
+ye command new terminal pe and sql vs code se access
+/usr/local/mysql/bin/mysql -u root -p and than password new line mei 
+
+and file run ek baad connection close ke liye
+connection.end()
+
+topic 3
+sql query methode 1 by benchmark
+methode 2 mysql package and node.js
+methode 3 cli se new teminal query access
+methode 4 using sql file than same cli terminal use database than write source filename.sql*/
 
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'college',
+  password: 'Deep@2005',
+});
 
-let randomuser=()=> {
+/* let query="SHOW TABLES"
+try{
+connection.query(query,(err,result)=>{
+  if(err) throw err
+  console.log(result)
+})
+}
+catch(err){
+  console.log(err)
+}*/
+
+
+// inserting new data
+let q="insert into newstudent (id,username,email,password) values ?;"
+let newstudentsss=[["343d","newuser1d","ddeep@gmail.com","dabc"],["343b","newuser1b","bdeep@gmail.com","babc"]]
+
+try{
+connection.query(q,[newstudentsss
+],(err,result)=>{
+  if(err) throw err
+  console.log(result)
+})
+}
+catch(err){
+  console.log(err)
+}
+
+connection.end()
+
+let getrandomuser=()=> {
  return {
    userId: faker.string.uuid(),
    username: faker.internet.username(),
    email: faker.internet.email(),
-   avatar: faker.image.avatar(),
    password: faker.internet.password(),
-   birthdate: faker.date.birthdate(),
-   registeredAt: faker.date.past(),
  };
 }
-
-console.log(randomuser)
+// console.log(getrandomuser())
