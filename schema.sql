4aa09778f47aef1e6aee32b5a11a7792d3c84c69
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-05-13 23:57:51 +0530

    all files

---
+++ schema.sql
@@ -0,0 +1,10 @@
+-- create table newstudent(
+--     id varchar(50) primary key,
+--     username varchar(50) unique,
+--     email varchar(50) unique not null,
+--     password varchar(50) unique not null
+-- );
+-- show tables;
+
+-- insert  into newstudent values("askk","deep","aaa@gmail.com","aaabbb")
+
